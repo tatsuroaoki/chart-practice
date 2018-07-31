396d02e7e38bbb57a458e03f0145b6d049f2c7c5
Cheri Allen <cherimarie@gmail.com>
2018-07-30 19:44:22 -0700

    Allocations chart added, working

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-// Load the Visualization API and the corechart package.
 google.charts.load('current', {'packages':['corechart']});
 
-// Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawAllocationChart);
 
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
 function drawChart() {
-
-  // Create the data table.
-  // two times 0-100, five times 100-200, three times 200-300
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'Score Range');
   data.addColumn('number', 'Games');
@@ -20,12 +13,32 @@ function drawChart() {
     ['200-300', 3]
   ]);
 
-  // Set chart options
   var options = {'title':'Bowling Score Incidences',
                  'width':400,
                  'height':300};
 
-  // Instantiate and draw our chart, passing in some options.
   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
+
+function drawAllocationChart() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Expense');
+  data.addColumn('number', 'Cents');
+  data.addRows([
+    ['Administrative Costs', 4],
+    ['Fundraising', 16],
+    ['Youth Programs', 36],
+    ['Adult Programs', 44]
+  ]);
+
+  var options = {'title':'Each Dollar Donated Goes To',
+                 'width':400,
+                 'height':300,
+                 'pieHole': 0.5,
+                  'colors': ['#8AD1C2', '#9F8AD1', '#D18A99', '#BCD18A']
+                };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allocation'));
   chart.draw(data, options);
 }
