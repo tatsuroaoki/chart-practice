6e48844927502e018e81556011f9899d35486d23
tatsuroaoki <tatsuro.aoki@gmail.com>
2018-07-30 19:09:37 -0700

    changed chart to represent bowling score frequency

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 console.log("let's build some charts!")
 
-			// Load the Visualization API and the corechart package.
-			google.charts.load('current', {'packages':['corechart']});
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
 
-			// Set a callback to run when the Google Visualization API is loaded.
-			google.charts.setOnLoadCallback(drawChart);
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
 
-			// Callback that creates and populates a data table,
-			// instantiates the pie chart, passes in the data and
-			// draws it.
-			function drawChart() {
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
 
-				// Create the data table.
-				var data = new google.visualization.DataTable();
-				data.addColumn('string', 'Topping');
-				data.addColumn('number', 'Slices');
-				data.addRows([
-					['Mushrooms', 3],
-					['Pepper', 2],
-					['Olives', 1],
-					['Sausage', 3],
-					['Pepperoni', 2]
-				]);
+	// Create the data table.
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Score Range');
+	data.addColumn('number', 'Frequency');
+	data.addRows([
+		['0-100', 2],
+		['100-200', 5],
+		['200-300', 3]
+//		['Sausages', 3],
+//		['Pepperoni', 2]
+	]);
 
-				// Set chart options
-				var options = {'title':'How Many Slices of Pizza I Ate Last Night By Topping',
-																			'width':400,
-																			'height':300};
+	// Set chart options
+	var options = {'title':'My Bowling Score Frequency',
+																'width':400,
+																'height':300};
 
-				// Instantiate and draw our chart, passing in some options.
-				var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-				chart.draw(data, options);
-			}
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+}
