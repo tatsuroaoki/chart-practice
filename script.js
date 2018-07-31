c203a866255389408d44f6602dfd8f7bf655103d
Cheri Allen <cherimarie@gmail.com>
2018-07-30 18:58:59 -0700

    Charts loading and looking good

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("let's build some charts!")
+
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 1],
+          ['Meatball', 1],
+          ['Banana Peppers', 3],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'What Kinds of Pizza I Ate Yesterday',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
